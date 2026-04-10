c1b1abd573e5c6e244db4432aef9872d0ca3374f
Feli-Veiga <feveiga@uade.edu.ar>
2026-04-10 17:43:11 -0300

    Clase 10/04

--- e-commerce/src/main/java/com/uade/tpo/e_commerce/exception/GlobalExceptionHandler.java
+++ e-commerce/src/main/java/com/uade/tpo/e_commerce/exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GolbalExceptionHandler {
+public class GlobalExceptionHandler {
     
     @ExceptionHandler(UsuarioNotFoundException.class)
     public ResponseEntity<String> handleUsuarioNotFound(UsuarioNotFoundException ex) {
