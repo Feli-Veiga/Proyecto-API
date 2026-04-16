5b206d467e6a5dbaa6fb828d0edd86037045d4a1
Juan Cruz <Juancruzdehezacorona@uade.edu.ar>
2026-04-16 11:36:31 -0300

    Agrego documentación en UsuarioRequestDTO

--- e-commerce/src/main/java/com/uade/tpo/e_commerce/dto/UsuarioRequestDTO.java
+++ e-commerce/src/main/java/com/uade/tpo/e_commerce/dto/UsuarioRequestDTO.java
@@ -3,6 +3,9 @@ package com.uade.tpo.e_commerce.dto;
 import java.time.LocalDate;
 
 import lombok.Data;
+//DTO utilizado para recibir los datos de creación de un usuario desde el cliente.
+//Esta clase se usa como entrada en los endpoints y luego es procesada en la capa de servicio.
+
 @Data
 public class UsuarioRequestDTO {
     
